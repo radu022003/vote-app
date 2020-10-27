97caacf4c3adb77f62a28d4511a5116ac1f2aad0
Radu-Nicolae Botezatu <Radu.Botezatu@partner.bmw.de>
2020-10-27 14:22:03 +0100

    added animation

--- src/app/app-main/app-main.component.ts
+++ src/app/app-main/app-main.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { CandidateService } from '../candidate.service';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-app-main',
   templateUrl: './app-main.component.html',
   styleUrls: ['./app-main.component.css'],
-  providers: [CandidateService]
+  providers: [CandidateService],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [ // each time the binding value changes
+        query(':leave', [
+          stagger(500, [
+            animate('0.9s', style({ opacity: 0 }))
+          ])
+        ], { optional: true }),
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(500, [
+            animate('0.5s', style({ opacity: 1 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class AppMainComponent {
-  Urls: string[];
+  Urls = [];
+  items = [];
+
   constructor(candidateList: CandidateService) {
     this.Urls = candidateList.getUrls();
+    this.toggle();
    }
+
+   logAnimation(event: any): void {
+    console.log(event);
+  }
+   showItems(): void {
+    this.Urls.map((i) => {
+      this.items.push(i);
+    });
+
+  }
+
+  hideItems(): void {
+    this.items = [];
+  }
+
+  toggle(): void {
+    this.items.length ? this.hideItems() : this.showItems();
+  }
 }
